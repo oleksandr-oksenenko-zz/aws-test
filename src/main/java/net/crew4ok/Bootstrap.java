3be0a808fe89bbf756ddd0ea2e2a422d361951fb
Aleksandr Oksenenko <crew4ok@gmail.com>
2015-05-09 15:33:45 +0300

    Add db

--- src/main/java/net/crew4ok/Bootstrap.java
+++ src/main/java/net/crew4ok/Bootstrap.java
@@ -1,6 +1,11 @@
 package net.crew4ok;
 
+import org.postgresql.Driver;
 import spark.servlet.SparkApplication;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 import static spark.Spark.get;
 
@@ -17,5 +22,36 @@ public class Bootstrap implements SparkApplication {
 
     private static void initRoutes() {
         get("/", (req, res) -> "Hello, world!");
+        get("/db", (req, res) -> {
+            Class.forName("org.postgresql.Driver");
+
+            String jdbcConnectionString = getJdbcConnectionString();
+            System.out.println(jdbcConnectionString);
+
+            Connection conn = null;
+            try {
+                conn = DriverManager.getConnection(jdbcConnectionString);
+
+                if (conn != null) {
+                    return "db works!";
+                }
+            } finally {
+                if (conn != null) {
+                    conn.close();
+                }
+            }
+        });
+    }
+
+    private static String getJdbcConnectionString() {
+        String dbName = System.getProperty("RDS_DB_NAME");
+        String userName = System.getProperty("RDS_USERNAME");
+        String password = System.getProperty("RDS_PASSWORD");
+        String hostname = System.getProperty("RDS_HOSTNAME");
+        String port = System.getProperty("RDS_PORT");
+        String jdbcUrl = "jdbc:postgresql://" + hostname + ":" +
+                port + "/" + dbName + "?user=" + userName + "&password=" + password;
+
+        return jdbcUrl;
     }
 }
